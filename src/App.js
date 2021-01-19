c60b70dd95553d8a6a524f8ad0878492461beb8a
gonovahamza <you@example.com>
2021-01-19 20:47:11 +0500

    global state across multiLevel components in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import ComponentA from './Components/ComponentA';
+import ComponentB from './Components/ComponentB';
+import ComponentC from './Components/ComponentC';
+import React,{useReducer,useContext} from 'react';
+export const CountContext = React.createContext();
 function App() {
+
+  const initialState = 0;
+  const reducer = (state, action) => {
+    switch (action) {
+      case 'increment':
+        return state + 1;
+      case 'decrement':
+        return state - 1;
+      case 'reset':
+        return initialState;
+      default:
+        return state;
+    }
+  }
+
+  const [count,dispatch] = useReducer(reducer,initialState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CountContext.Provider value={{countState:count,countDispatch:dispatch}}>
+      <div className="App">
+        Count - {count}
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
+
   );
 }
 
